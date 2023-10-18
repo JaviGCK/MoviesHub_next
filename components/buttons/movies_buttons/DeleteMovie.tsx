3bf916b702e678c2e43cb3b1119a14327efb9eea
Javier García <javigcpgr@gmail.com>
2023-10-18 11:28:49 +0200

    feat(refresh)

--- components/buttons/movies_buttons/DeleteMovie.tsx
+++ components/buttons/movies_buttons/DeleteMovie.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import { deleteMovie } from '@/actions/movie.action';
 import styles from './buttons_movies.module.css';
 
@@ -8,14 +8,16 @@ import { BsTrash3Fill } from 'react-icons/bs';
 
 type Props = {
     movieId: number;
+    onActionSuccess: () => void; // Actualizada para aceptar onActionSuccess
 };
 
-const DeleteMovie = ({ movieId }: Props) => {
+const DeleteMovie = ({ movieId, onActionSuccess }: Props) => {
     const router = useRouter();
 
     const handleDeleteClick = async () => {
         try {
             await deleteMovie(movieId);
+            onActionSuccess(); // Llamar a la función para indicar éxito
             router.refresh();
         } catch (error) {
             console.error('Error deleting movie:', error);
